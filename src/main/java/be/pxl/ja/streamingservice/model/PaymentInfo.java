c1329aa36f82fc7d4c8f1c2e28dbe653c4727cf9
MaudeVanhengelPXL <maude.vanhengel@student.pxl.be>
2020-11-08 19:53:40 +0100

    Added PaymentInfo class and InvalidDateException

--- src/main/java/be/pxl/ja/streamingservice/model/PaymentInfo.java
+++ src/main/java/be/pxl/ja/streamingservice/model/PaymentInfo.java
@@ -1,4 +1,47 @@
 package be.pxl.ja.streamingservice.model;
 
+import be.pxl.ja.streamingservice.exception.InvalidDateException;
+
+import java.time.LocalDate;
+import java.time.Period;
+
 public class PaymentInfo {
+    private String firstname;
+    private String lastname;
+    private CreditCardNumber cardNumber;
+    private LocalDate expirationDate;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public CreditCardNumber getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(CreditCardNumber cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        if (Period.between(LocalDate.now(), expirationDate).getMonths() < 0) {
+            throw new InvalidDateException(expirationDate, "expiration date", "Must be valid for at least 1 month.");
+        }
+    }
 }
